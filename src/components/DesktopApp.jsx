4860631b5dc1219982f6ba1861b6987698cad556
nonso01 <chukwunonsomartin01@gmail.com>
2025-07-08 15:54:14 +0100

    still on the header

--- src/components/DesktopApp.jsx
+++ src/components/DesktopApp.jsx
@@ -3,7 +3,7 @@ import Connecting from "./Connecting";
 import Logo from "/images/favicon.png";
 import { createScope, createDraggable, createSpring } from "animejs";
 import { useRef, useState, useEffect } from "react";
-import { Star } from "lucide-react";
+import { Star, ArrowUpRight } from "lucide-react";
 
 export default function DesktopApp({ connected, onSetConnected }) {
   const root = useRef(null);
@@ -64,46 +64,50 @@ export default function DesktopApp({ connected, onSetConnected }) {
               </span>
             </div>
           </nav>
-          <section className="d-s1 flex column evenly debbug">
-            <div className="intro-name flex column evenly debbug">
+          <section className="d-s1 flex column evenly ">
+            <div className="intro-name flex column evenly ">
               <p className="p-hello flex center">
                 <span>HELLO</span>
               </p>
               <p className="p-name">
                 I'm <span>Martin,</span>
-                <p>Frontend Developer</p>
+                Frontend Developer
               </p>
             </div>
 
-            <div className="intro-rating flex between debbug">
-              <div className="debbug">
-                <p>''</p>
-                <p>
+            <div className="intro-rating flex between ">
+              <div className=" flex column between">
+                <p className="p-quote">❝</p>
+                <p className="p-info">
                   Martin's proffesionalism and expertise ensured our website and
                   brand success. Highly recommended!
                 </p>
-                <p>
-                  <span>30+</span>
-                  <span>Client Served</span>
-                </p>
+                <div className="p-client">
+                  <p>30+</p>
+                  <p>Client Served</p>
+                </div>
               </div>
 
-              <div className="debbug">
-                <p>
+              <div className=" flex column end">
+                <p className="p-stars flex evenly">
                   <Star fill="var(--light-green)" stroke="var(--light-green)" />
                   <Star fill="var(--light-green)" stroke="var(--light-green)" />
                   <Star fill="var(--light-green)" stroke="var(--light-green)" />
                   <Star fill="var(--light-green)" stroke="var(--light-green)" />
                   <Star fill="var(--light-green)" stroke="var(--light-green)" />
                 </p>
-                <p>4 Years</p>
-                <p>In The Making!</p>
+                <p className="p-years">
+                  <span>4</span> Years
+                </p>
+                <p className="p-ex">Expert</p>
               </div>
             </div>
 
-            <div className="intro-links debbug">
-              <p>Portfolio</p>
-              <p>Hire me</p>
+            <div className="intro-links flex evenly ">
+              <p className="flex center">
+                Portfolio <ArrowUpRight />
+              </p>
+              <p className="flex center">Hire me</p>
             </div>
           </section>
         </header>
@@ -113,7 +117,7 @@ export default function DesktopApp({ connected, onSetConnected }) {
             height: 1020px;
 
             a {
-              // font-size: 1.1rem;
+              font-size: 1.1rem;
               font-weight: bolder;
               &:hover {
                 color: var(--light-green);
@@ -153,7 +157,6 @@ export default function DesktopApp({ connected, onSetConnected }) {
                 width: 72%;
 
                 .p-hello {
-                  // border: 2px solid;
                   align-self: center;
                   width: 100px;
                   font-weight: bold;
@@ -164,6 +167,7 @@ export default function DesktopApp({ connected, onSetConnected }) {
                 }
 
                 .p-name {
+                  /*.p-name p */
                   font-size: 5.5rem;
                   text-align: center;
                   font-weight: bold;
@@ -176,13 +180,83 @@ export default function DesktopApp({ connected, onSetConnected }) {
 
               .intro-rating {
                 height: 300px;
+                // p {
+                //   border: 2px solid;
+                // }
                 div {
                   width: 400px;
+                }
+
+                .p-quote {
+                  font-size: 3rem;
+                }
+
+                .p-info {
+                  font-size: 1.3rem;
+                  text-wrap: balance;
+                  line-height: 1.5;
+                }
+
+                .p-client {
+                  p:nth-child(1) {
+                    font-size: 2rem;
+                    font-weight: bold;
+                    font-family: "Orbitron";
+                  }
+                  p:nth-child(2) {
+                    margin-top: 0.6rem;
+                    font-size: var(--font-sm);
+                  }
+                }
+
+                .p-stars {
+                  width: 50%;
+                }
+
+                .p-years {
+                  font-size: 2rem;
+                  font-weight: bold;
+                  margin-block: 2rem;
+                  span {
+                    font-family: "Orbitron";
+                  }
+                }
+
+                .p-ex {
+                  font-size: 2.5rem;
+                  text-align: right;
+                  padding-bottom: 1.5rem;
+                  width: 80%;
+                  border-bottom: 5px solid;
                 }
               }
 
               .intro-links {
                 height: 100px;
+                width: 500px;
+                align-self: center;
+                // border-radius: 25px;
+                // background-color: var(--light-dark);
+
+                p:nth-child(1) {
+                  background-color: var(--light-green);
+                }
+
+                p:nth-child(2) {
+                  // background-color: var(--light-green);
+                  color: var(--light-green);
+                  border: 2px solid;
+                }
+
+                p {
+                  width: 45%;
+                  align-self: center;
+                  height: 80px;
+                  border-radius: 25px;
+                  font-size: 1.7rem;
+                  gap: 0.625rem;
+                  cursor: pointer;
+                }
               }
             }
           }
