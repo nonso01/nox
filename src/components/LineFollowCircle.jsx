fbea2523cea07ff7e8a95e02ea0cbe8b4d55b117
nonso01 <chukwunonsomartin01@gmail.com>
2025-07-11 11:55:03 +0100

    work experience

---
+++ src/components/LineFollowCircle.jsx
@@ -0,0 +1,24 @@
+export default function LineFollowCircle() {
+  return (
+    <svg
+      width="50"
+      height="494"
+      viewBox="0 0 50 494"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_119_39)">
+        <path d="M25 48L25 224" stroke="#34db69" stroke-width="4" />
+        <path d="M25 270L25 446" stroke="#34db69" stroke-width="4" />
+        <circle cx="25" cy="25" r="23" stroke="#34db69" stroke-width="4" />
+        <circle cx="25" cy="247" r="23" stroke="#34db69" stroke-width="4" />
+        <circle cx="25" cy="469" r="23" stroke="#34db69" stroke-width="4" />
+      </g>
+      <defs>
+        <clipPath id="clip0_119_39">
+          <rect width="50" height="494" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
