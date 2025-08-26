655912bab2133435c116ebb8f83208282f8ef844
nonso01 <chukwunonsomartin01@gmail.com>
2025-08-26 16:24:36 +0100

    update port

--- rs-server/src/main.rs
+++ rs-server/src/main.rs
@@ -7,8 +7,8 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Server running on http://127.0.0.1:7878");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server running on http://127.0.0.1:8080");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
