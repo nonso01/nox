0dcd519bd59aa438ae624c0ef4664a4937b3ea68
nonso01 <chukwunonsomartin01@gmail.com>
2025-09-28 12:32:43 +0100

    dev tags

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import "./components/ui-shared/user-flow.css"
 import App from './App.jsx'
 
+window._NOX_MODE_ = import.meta.env.MODE ?? "development"; 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
