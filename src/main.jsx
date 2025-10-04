0399ff02e1d5e528b0ae39842e5511eb0cb57da0
nonso01 <chukwunonsomartin01@gmail.com>
2025-10-04 21:58:47 +0100

    mark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import "./components/ui-shared/user-flow.css"
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./components/ui-shared/user-flow.css";
+import App from "./App.jsx";
 
-window._NOX_MODE_ = import.meta.env.MODE ?? "development"; 
+window._NOX_MODE_ = import.meta.env.MODE ?? "development";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    {_NOX_MODE_ ? (
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          width: 20,
+          height: 20,
+          margin: "10px",
+          borderRadius: "50%",
+          color: "var(--nox-danger-color)",
+          border: "2px solid currentColor",
+          // opacity: 0.8,
+          backgroundColor: "currentColor",
+          zIndex: 99999,
+        }}
+        title="This Website is still under heavy development"
+      ></div>
+    ) : (
+      void 0
+    )}
+  </StrictMode>
+);
